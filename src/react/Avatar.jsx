e4b1b07e39b09fde8ed41accbf4825591fd73864
gavinhanzel <gavinhanzel@gmail.com>
2026-05-02 00:05:09 +0000

    Avatar class to use rather than repeat default avatar lines

---
+++ src/react/Avatar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DEFAULT_AVATAR_URL = 'https://firebasestorage.googleapis.com/v0/b/bonfire-d8db1.firebasestorage.app/o/Profile_Pictures%2Flogo.png?alt=media&token=15ac7dfc-d970-49f2-a9c6-429dd0656f0a';
+
+const Avatar = ({ src, alt, className }) => {
+  return (
+    <img
+      src={src || DEFAULT_AVATAR_URL}
+      alt={alt || 'Avatar'}
+      className={className}
+    />
+  );
+};
+
+export default Avatar;
