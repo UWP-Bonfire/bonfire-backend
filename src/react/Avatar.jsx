ee1967d099881e36a9067f5dc50d0c0a4db56bf2
gavinhanzel <gavinhanzel@gmail.com>
2026-05-02 00:09:17 +0000

    Refactoring messages to use avatar.jsx

--- src/react/Avatar.jsx
+++ src/react/Avatar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const DEFAULT_AVATAR_URL = 'https://firebasestorage.googleapis.com/v0/b/bonfire-d8db1.firebasestorage.app/o/Profile_Pictures%2Flogo.png?alt=media&token=15ac7dfc-d970-49f2-a9c6-429dd0656f0a';
 
-const Avatar = ({ src, alt, className }) => {
+const Avatar = ({ src, alt, className, onClick }) => {
   return (
     <img
       src={src || DEFAULT_AVATAR_URL}
       alt={alt || 'Avatar'}
       className={className}
+      onClick={onClick}
     />
   );
 };
